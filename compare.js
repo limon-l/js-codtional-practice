cd048e90fb6aecd53cb05c92b715effe3b5650d0
limon-l <limonroyapu.edu@gmail.com>
2025-07-28 12:41:48 +0600

    practice 1 done and other files added

---
+++ compare.js
@@ -0,0 +1,23 @@
+// console.log(5 = 9); //Invalid(Syntax Error)
+// console.log(5 == 9);
+// console.log(5 > 9);
+// console.log(5 < 9);
+// console.log(5 <= 9);
+// console.log(5 >= 9);
+// console.log(5 != 9);
+
+// console.log(5 == '5');
+// console.log(5 === '5');
+
+
+var age = 45;
+
+if (age >= 0 && age <= 18) {
+    console.log("He is Child");
+}
+else if(age >= 18){
+    console.log("He is Adult");
+}
+else {
+    console.log("Invalid Input");
+}
---
+++ practice1.js
@@ -0,0 +1,20 @@
+// Free Drinks
+//     - Burger more than 500tk: free Coke
+//     - Else Coke: 30tk
+
+
+let burgerPrice = 501;
+if (burgerPrice>500) {
+    console.log("Yes, You won a COKE Free!");
+} else {
+    console.log("Sorry, You Have to pay tk.30 to get a COKE");
+}
+
+
+
+// let burgerPrice = 399;
+// if (burgerPrice>500) {
+//     console.log("Yes, You won a COKE Free!");
+// } else {
+//     console.log("Sorry, You Have to pay tk.30 to get a COKE");
+// }
